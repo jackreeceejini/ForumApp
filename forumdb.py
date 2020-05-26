a2a5ee5cdd1827cedff5a291951b2181bb6a9d21
jackreeceejini <jackreeceejini@gmail.com>
2020-05-26 16:02:32 -0400

    added get posts

--- forumdb.py
+++ forumdb.py
@@ -1,15 +1,18 @@
 # "Database code" for the DB Forum.
+import psycopg2
 
-import datetime
-
-POSTS = [("This is the first post.", datetime.datetime.now())]
+DBNAME = "forum"
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  db = psycopg2.connect(database=DBNAME)
+  c = db.cursor()
+  c.execute("select content, time from posts order by time desc")
+  return c.fetchall()
+  db.close()
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
+  
 
 
